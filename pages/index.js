1c72ec5a37a109061c06b56a0cd379eaed3087fd
Dzikri Qalam Hatorangan <dzikri.qalam01@ui.ac.id>
2022-11-02 01:45:03 +0700

    feat: implement real time crypto data

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,26 @@ import Hero from "../ui/components/Hero"
 import LiveCrypto from '../ui/components/LiveCrypto'
 
 
-export default function Home() {
+export default function Home({ data }) {
   return (<>
     <Hero />
-    <LiveCrypto />
+    <LiveCrypto cryptoData={data} />
     <Features />
     <CTA />
     <Footer />
   </>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=5`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CMC_PRO_API_KEY': 'adfb958b-fb38-4c65-9a5f-f67e7b36e3a5',
+    },
+  })
+  const data = await res.json()
+
+  return { props: { data } }
 }
--- ui/components/LiveCrypto/index.jsx
+++ ui/components/LiveCrypto/index.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { Box, Flex, Image, HStack, Text, VStack } from '@chakra-ui/react'
 import { ButtonPrimary, ButtonSecondary } from '../Button'
 
-function CryptoItem() {
+function CryptoItem({ name, id, price, change }) {
     return (<>
         <Box display={{ base: 'none', md: 'block' }}>
-            <HStack justify={'space-between'} mb='39px' >
-                <Text fontSize={{ base: '16px', lg: '32px' }} color='#D1D0D1'>Bitcoin</Text>
-                <Text fontSize={{ base: '16px', lg: '24px' }} color='#7D7D7D'>BTC</Text>
-                <Text fontSize={{ base: '16px', lg: '32px' }} color='#F4F4F4'>$20,957.32</Text>
-                <Text fontSize='16px' color='#20BCA4'>+0.13</Text>
-                <Image w='145px' h='30px' src='/assets/Decor.svg' />
+            <HStack justify={'space-between'} alignItems='center' mb='39px' >
+                <Text fontSize={{ base: '16px', lg: '32px' }} color='#D1D0D1'>{name}</Text>
+                <Text fontSize={{ base: '16px', lg: '24px' }} color='#7D7D7D'>{id}</Text>
+                <Text fontSize={{ base: '16px', lg: '32px' }} color='#F4F4F4'>${price}</Text>
+                <Text fontSize='16px' color='#20BCA4'>{change}</Text>
+                {/* <Image w='145px' h='30px' src='/assets/Decor.svg' /> */}
                 <ButtonPrimary text='Trade' />
             </HStack>
             <Box h='1px' bg='rgba(244, 244, 244, 0.3)' mb='39px' />
@@ -18,12 +18,12 @@ function CryptoItem() {
         <Box display={{ base: 'block', md: 'none' }}>
             <HStack justify={'space-between'} mb='20px' >
                 <VStack align={'flex-start'}>
-                    <Text fontSize={{ base: '16px', lg: '32px' }} color='#D1D0D1'>Bitcoin</Text>
-                    <Text fontSize={{ base: '16px', lg: '24px' }} color='#7D7D7D'>BTC</Text>
+                    <Text fontSize={{ base: '16px', lg: '32px' }} color='#D1D0D1'>{name}</Text>
+                    <Text fontSize={{ base: '16px', lg: '24px' }} color='#7D7D7D'>{id}</Text>
                 </VStack>
                 <VStack align={'flex-end'}>
-                    <Text fontSize={{ base: '16px', lg: '32px' }} color='#F4F4F4'>$20,957.32</Text>
-                    <Text fontSize='16px' color='#20BCA4'>+0.13</Text>
+                    <Text fontSize={{ base: '16px', lg: '32px' }} color='#F4F4F4'>${price}</Text>
+                    <Text fontSize='16px' color='#20BCA4'>{change}</Text>
                 </VStack>
                 <ButtonPrimary text='Trade' />
             </HStack>
@@ -33,13 +33,11 @@ function CryptoItem() {
     )
 }
 
-function CryptoList() {
+function CryptoList({ allCrypto }) {
+    console.log(allCrypto);
     return (
         <Box bg='#131415' w={{ base: '95%', md: '80%' }} borderRadius={'25px'} p={{ base: '30px', md: '65px' }} mt='63px'>
-            <CryptoItem />
-            <CryptoItem />
-            <CryptoItem />
-            <CryptoItem />
+            {allCrypto.data.map(ctx => <CryptoItem key={ctx.symbol} name={ctx.name} id={ctx.symbol} price={ctx.quote.USD.price.toString().slice(0, 8)} change={ctx.quote.USD.volume_change_24h.toString().slice(0, 6)} />)}
             <Flex justify={'center'}>
                 <ButtonSecondary text='Learn More' />
             </Flex>
@@ -47,13 +45,14 @@ function CryptoList() {
     )
 }
 
-function LiveCrypto() {
+function LiveCrypto({ cryptoData }) {
     return (
         <Flex flexDirection={'column'} justify='center' align='center' position='relative'>
-            <CryptoList />
+            <CryptoList allCrypto={cryptoData} />
             <Image src={'/assets/Decor.svg'} w='full' bottom='-12.5vw' position={'absolute'} zIndex='-1' />
         </Flex>
     )
 }
 
 export default LiveCrypto
+
