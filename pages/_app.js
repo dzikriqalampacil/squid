a3c227a85aaa08241ccbc2acaa27d383f0834a0c
Dzikri Qalam Hatorangan <dzikri.qalam01@ui.ac.id>
2022-11-02 21:48:42 +0700

    feat: add seo

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,25 @@ import '@fontsource/poppins/500.css'
 import '@fontsource/poppins/600.css'
 import '@fontsource/poppins/700.css'
 import '@fontsource/poppins/800.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+
+        <title>Squid</title>
+        <meta name='description' content="The easiest place to buy and sell cryptocurrency today." />
+
+        <meta property="og:image" content="/assets/SquidLogo.jpg" />
+        <meta property="og:title" content="Squid" />
+        <meta property="og:description" content="The easiest place to buy and sell cryptocurrency today." />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
